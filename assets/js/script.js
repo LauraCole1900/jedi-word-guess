80e0bb3fe035ce219bcc4dd55e69862a65e9f63a
Laura Cole <lauracole1900@comcast.net>
2021-09-23 21:06:50 -0600

    added word array & random number generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,12 @@
-// # Unit 04 Mini-Project: Word Guess
-
-// In this activity, you will work with a group to build a game using JavaScript and Web APIs.
-
 // ## Instructions
 
 // The completed application should meet the following criteria:
 
 // * As a user, I want to start the game by clicking on a button. 
-
 // * As a user, I want to try and guess a word by filling in a number of blanks that match the number of letters in that word.
-
 // * As a user, I want the game to be timed. 
-
 // * As a user, I want to win the game when I have guessed all the letters in the word.
-
 // * As a user, I want to lose the game when the timer runs out before I have guessed all the letters.
-
 // * As a user, I want to see my total wins and losses on the screen. 
 
 // ### Specifications
@@ -30,10 +21,6 @@
 
 // * When a user refreshes or returns to the brower page, the win and loss counts should persist.
 
-// ## 💡 Notes
-
-// Refer to the documentation:
-
 // * [MDN Web Docs on KeyboardEvent](https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent)
 
 // * [MDN Web Docs on Array](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array)
@@ -46,14 +33,23 @@
 
 // * [MDN Web Docs on localStorage](https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage)
 
-// ## 🏆 Bonus
+// * How can you add a reset button to set the win and loss counts back to zero?
 
-// If you have completed this activity, work through the following challenge with your group to further your knowledge:
+// Needs an array of words
+// Words are randomly selected from the array -> random number generator
+// Needs a timer
+// Needs to show wins & losses
+// Needs variables to store time elapsed, wins, losses
+// Needs to generate a number of blanks equal to the length of the word
+// Needs to detect key events -> keyup? keydown?
+// If the character code for the key pressed is included in the word, blank(s) turn(s) to that letter
+// .includes() and indexOf(), probably
+// also, .toLowerCase()?
 
-// * How can you add a reset button to set the win and loss counts back to zero? 
 
-// Use [Google](https://www.google.com) or another search engine to research this.
+// Global variables
+const wordArr = ["sith lord", "tatooine", "luke skywalker", "millenium falcon", "leia organa", "kylo ren", "ewoks", "wookiee", "grogu", "boba fett", "lightsaber", "jedi knight", "padawan", "coruscant", "ahsoka tano", "din djarin", "mandalorian", "darth vader", "anakin skywalker", "the force", "chewbacca", "yoda", "endor", "tusken raider", "obi-wan kenobi", "han solo", "death star", "tie fighter", "alderaan", "lando calrissian", "mos eisley", "jabba the hutt", "sarlacc", "droid", "kuiil", "moff gideon"]
 
-// ---
-
-// © 2021 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
+const wordIndex = () => {
+  Math.floor(Math.random() * wordArr.length)
+}
