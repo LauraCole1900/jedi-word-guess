94c03eb43baf85878b053e93a54ae2a0b262a734
Laura Cole <lauracole1900@comcast.net>
2022-03-19 14:25:04 -0600

    added to word bank; trying to deal with space-bar bug; trying to trigger virtual keyboard on mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
   let word = "";
   let wordToGuessArr = [];
   let blankArr = [];
-  const wordArr = ["sith lord", "tatooine", "luke skywalker", "millenium falcon", "leia organa", "kylo ren", "ewoks", "wookiee", "grogu", "boba fett", "lightsaber", "jedi knight", "padawan", "coruscant", "ahsoka tano", "din djarin", "mandalorian", "darth vader", "anakin skywalker", "the force", "chewbacca", "yoda", "endor", "tusken raider", "obi-wan kenobi", "han solo", "death star", "tie fighter", "alderaan", "lando calrissian", "mos eisley", "jabba the hutt", "sarlacc", "droid", "kuiil", "moff gideon"];
+  const wordArr = ["sith lord", "tatooine", "luke skywalker", "millenium falcon", "leia organa", "kylo ren", "ewok", "wookiee", "grogu", "boba fett", "lightsaber", "jedi knight", "padawan", "coruscant", "ahsoka tano", "din djarin", "mandalorian", "darth vader", "anakin skywalker", "the force", "chewbacca", "yoda", "endor", "tusken raider", "obi-wan kenobi", "han solo", "death star", "tie fighter", "alderaan", "lando calrissian", "mos eisley", "jabba the hutt", "sarlacc", "droid", "kuiil", "moff gideon", "x-wing fighter", "blaster", "clone wars", "empire", "rebel alliance", "galaxy", "star cruiser", "stormtrooper", "tauntaun", "padme amidala", "uncle owen", "aunt beru", "palpatine", "jawa", "darth maul", "darth sidious", "dagobah", "midi-chlorians", "beskar", "hyperdrive", "bantha", "admiral ackbar", "rancor", "count dooku", "darksaber", "mace windu", "hoth", "naboo", "mudhorn", "porg", "wampa", "greebo", ""];
 
   // Gets data from local storage on page load and sets data to wins & losses variables
   // Hides the gameBoard div on page load
@@ -71,21 +71,23 @@ $(document).ready(function () {
   }
 
   const detectKeypress = (e) => {
-    if (!wordToGuessArr.includes(e.key) && !["Shift", " ",].includes(e.key)) {
-      time -= 3;
-    }
-    if (!blankArr.includes(e.key) && e.key !== " " && time > 0) {
-      for (let i = 0; i < wordToGuessArr.length; i++) {
-        if (wordToGuessArr[i] === e.key.toLowerCase()) {
-          $(`#${i}`).text(e.key.toLowerCase());
-          blankArr = blankArr.concat(e.key);
-          blankArr.shift();
-        }
-        continue;
+    if (e.key !== " ") {
+      if (!wordToGuessArr.includes(e.key) && e.key !== "Shift") {
+        time -= 3;
       }
-    }
-    if (!blankArr.includes("_")) {
-      winGame();
+      if (!blankArr.includes(e.key) && e.key !== " " && time > 0) {
+        for (let i = 0; i < wordToGuessArr.length; i++) {
+          if (wordToGuessArr[i] === e.key.toLowerCase()) {
+            $(`#${i}`).text(e.key.toLowerCase());
+            blankArr = blankArr.concat(e.key);
+            blankArr.shift();
+          }
+          continue;
+        }
+      }
+      if (!blankArr.includes("_")) {
+        winGame();
+      }
     }
   }
 
@@ -119,7 +121,7 @@ $(document).ready(function () {
 
   $(document).keyup(detectKeypress);
 
-  $(startButton).on("click", startGame);
+  $(startButton).on("click", startGame).focus();
 
   $(clearButton).on("click", clearScores);
 });
